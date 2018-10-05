186f736dab86e55a8405ed4986d21a1633c2f4c1
circleGiven <circlegiven@gmail.com>
2018-10-05 17:16:16 +0900

    JWT 토큰 발급부 수정

--- src/main/java/com/example/demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -32,6 +32,9 @@ public class JwtTokenProvider {
     @Value("${app.jwtExpirationInMs}")
     private int jwtExpirationInMs;
 
+    @Value("${app.jwtRefreshExpirationInMs}")
+    private int jwtRefreshExpirationInMs;
+
     /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     | Public Variables
     |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
@@ -84,15 +87,17 @@ public class JwtTokenProvider {
 
                 .setExpiration(expiryDate)
                 .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                //
                 .compact();
     }
 
+    /**
+     *
+     * @param token
+     * @return
+     */
     public Authentication getAuthentication(String token) {
-        Claims claims = Jwts.parser()
-                .setSigningKey(jwtSecret)
-                .parseClaimsJws(token)
-                .getBody();
-
+        Claims claims = _getClaimsFromToken(token);
         Collection<? extends GrantedAuthority> authorities =
                 Arrays.stream(claims.get("auth").toString().split(","))
                     .map(SimpleGrantedAuthority::new)
@@ -109,85 +114,103 @@ public class JwtTokenProvider {
      * @return
      */
     public String createJWTToken(User user, String google_token, String google_refresh_token) {
-        // 현재 시간
-        Date now = new Date();
-        // 만료시간
-        Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
         // 토큰 정보
         Claims claims = Jwts.claims();
+        // 사용자 아이디
+        claims.put("id", user.getId());
+        // 사용자 이메일
         claims.put("email", user.getEmail());
+        // 사용자 이름
         claims.put("name", user.getName());
-        claims.put("admin", user.getAdminFlag());
-        claims.put("google_token", google_token);
-        claims.put("google_refresh_token", google_refresh_token);
+        // 사용자 어드민 권한 여부
+        claims.put("adminFl", user.getAdminFlag());
+        // 리프레시 토큰
+        claims.put("refresh_token", _generateRefreshToken(user, google_token, google_refresh_token));
+        // 구글 토큰이 있다면
+        if (google_token != null) {
+            claims.put("google_token", google_token);
+        }
+        // 구글 리프레시 토큰이 있다면
+        if (google_refresh_token != null) {
+            claims.put("google_refresh_token", google_refresh_token);
+        }
 
         return Jwts.builder()
                 // JWT token에 내려줄 정보 설정
                 .setClaims(claims)
-                // token이 발급된 시간
-                .setIssuedAt(new Date())
+                // 토큰의 이름 설정
+                .setSubject("access_token")
                 // token 만료시간
-                .setExpiration(expiryDate)
+                .setExpiration(new Date(jwtExpirationInMs))
                 // token 암호화
                 .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                //
                 .compact();
     }
 
 
     /**
-     * 토큰에서 사용자 이름 조회
+     * 토큰의 이름 얻기
+     * @param token
+     * @return
+     */
+    public String getSubjectFromJWT(String token) {
+        Claims claims = _getClaimsFromToken(token);
+        return claims.getSubject();
+    }
+
+    /**
+     * 토큰에서 사용자 아이디 얻기
+     * @param token
+     * @return
+     */
+    public String getUserIdFromJWT(String token) {
+        Claims claims = _getClaimsFromToken(token);
+        return (String) claims.get("id");
+    }
+
+    /**
+     * 토큰에서 사용자 이름 얻기
      * @param token
      * @return
      */
     public String getUserNameFromJWT(String token) {
-        Claims claims = Jwts.parser()
-                .setSigningKey(jwtSecret)
-                .parseClaimsJws(token)
-                .getBody();
+        Claims claims = _getClaimsFromToken(token);
         return (String) claims.get("name");
     }
 
     /**
-     * 토큰에서 사용자 이메일 조회
+     * 토큰에서 사용자 이메일 얻기
      * @param token
      * @return
      */
     public String getUserEmailFromJWT(String token) {
-        Claims claims = Jwts.parser()
-                .setSigningKey(jwtSecret)
-                .parseClaimsJws(token)
-                .getBody();
+        Claims claims = _getClaimsFromToken(token);
         return (String) claims.get("email");
     }
 
     /**
-     * 토큰에서 구글 토큰 조회
+     * 토큰에서 구글 토큰 얻기
      * @param token
      * @return
      */
     public String getGoogleTokenFromJWT(String token) {
-        Claims claims = Jwts.parser()
-                .setSigningKey(jwtSecret)
-                .parseClaimsJws(token)
-                .getBody();
+        Claims claims = _getClaimsFromToken(token);
         return (String) claims.get("google_token");
     }
 
     /**
-     * 토큰에서 구글 리프래시 토큰 조
+     * 토큰에서 구글 리프래시 토큰 얻기
      * @param token
      * @return
      */
     public String getGoogleRefreshTokenFromJWT(String token) {
-        Claims claims = Jwts.parser()
-                .setSigningKey(jwtSecret)
-                .parseClaimsJws(token)
-                .getBody();
+        Claims claims = _getClaimsFromToken(token);
         return (String) claims.get("google_refresh_token");
     }
 
     /**
-     * 토큰 유효성 체크
+     * JWT 토큰 유효성 체크
      * @param authToken
      * @return
      */
@@ -196,15 +219,15 @@ public class JwtTokenProvider {
             Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
             return true;
         } catch (SignatureException ex) {
-            logger.error("Invalid JWT signature");
+            logger.error("시그너처 연산이 실패하였거나, JWT 토큰의 시그너처 검증이 실패하였습니다");
         } catch (MalformedJwtException ex) {
-            logger.error("Invalid JWT token");
+            logger.error("구조적인 문제가 있는 JWT 토큰입니다");
         } catch (ExpiredJwtException ex) {
-            logger.error("Expired JWT token");
+            logger.error("유효 기간이 지난 JWT 토큰입니다");
         } catch (UnsupportedJwtException ex) {
-            logger.error("Unsupported JWT token");
+            logger.error("JWT 토큰의 형식이 애플리케이션에서 원하는 형식과 맞지 않습니다");
         } catch (IllegalArgumentException ex) {
-            logger.error("JWT claims string is empty.");
+            logger.error("JWT 토큰에 Claim이 비어있습니다");
         }
         return false;
     }
@@ -218,12 +241,54 @@ public class JwtTokenProvider {
     |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
 
     /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    | private Method
-    |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
-
-    /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     | Private Method
     |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
+
+    /**
+     * 토큰으로부터 claims 얻기
+     * @param token
+     * @return
+     */
+    private Claims _getClaimsFromToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    /**
+     * 리프레시 JWT token 생성
+     * @param user
+     * @param google_token
+     * @param google_refresh_token
+     * @return
+     */
+    private String _generateRefreshToken(User user, String google_token, String google_refresh_token) {
+        // 토큰 정보
+        Claims claims = Jwts.claims();
+        // 사용자 아이디
+        claims.put("id", user.getId());
+        // 구글 토큰이 있다면
+        if (google_token != null) {
+            claims.put("google_token", google_token);
+        }
+        // 구글 리프레시 토큰이 있다면
+        if (google_refresh_token != null) {
+            claims.put("google_refresh_token", google_refresh_token);
+        }
+
+        return Jwts.builder()
+                // JWT token에 내려줄 정보 설정
+                .setClaims(claims)
+                // 토큰의 이름 설정
+                .setSubject("refresh_token")
+                // token 만료시간
+                .setExpiration(new Date(jwtRefreshExpirationInMs))
+                // token 암호화
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                //
+                .compact();
+    }
 
     /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     | Inner Class
